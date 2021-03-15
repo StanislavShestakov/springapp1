40dc8546b69316801ba585f1edd3714bca687d0a
ShestakovStanislav <w3orks@mail.ru>
2021-03-15 14:25:30 +0300

    annotacion @value @PreDestroy @PostConstruct

--- src/main/java/ua/shestakov/springcourse/ClassicalMusic.java
+++ src/main/java/ua/shestakov/springcourse/ClassicalMusic.java
@@ -2,12 +2,23 @@ package ua.shestakov.springcourse;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 
 
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInint(){
+        System.out.println("Do my Inint");
+    }
+
+    @PreDestroy
+    public  void doMyDestroy(){
+        System.out.println("Do my Destroy");
+    }
 
     @Override
     public String getSong() {
